7aa4680e2cb847c4997c20774f7fdb3cda09840e
jinnk0 <jin8073@gmail.com>
2025-11-12 11:38:06 +0900

    eureka-server init

---
+++ src/test/java/org/pokeherb/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pokeherb.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
